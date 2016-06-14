c1a1ee12f34752e01f9dc5110b8e9bf8726eb23c
Alexey Berdnikov <aberdnikov@gmail.com>
2016-06-14 20:21:05 +0500

    refactoring

--- src/Route.php
+++ src/Route.php
@@ -13,46 +13,103 @@ class Route {
     const METHOD_DELETE             = 'delete';
     const METHOD_PATCH              = 'patch';
     const PATTERN_ANY               = '.+';
+    const PATTERN_WITH_SLASHES      = '(.*(?:%2F:)?.*)';
     const PATTERN_NUMERIC_TEXT      = '[\w\d]+';
     const PATTERN_NUMERIC_TEXT_DASH = '[\w-\d]+';
     const PATTERN_NUMERIC           = '\d+';
     const PATTERN_DATE              = '\d+\-\d+\-\d+';
     const PATTERN_EMAIL             = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}';
     static    $routes;
-    protected $code;
-    protected $model      = null;
-    protected $namespace  = null;
-    protected $domain     = null;
-    protected $middleware = [];
-    protected $base_url   = null;
+    static    $route_params = [];
+    protected $as;
+    protected $model        = null;
+    protected $namespace    = null;
+    protected $domain       = null;
+    protected $middleware   = [];
+    protected $base_url     = null;
+    protected $action;
+    protected $segments     = [];
+    protected $patterns     = [];
+    protected $controller   = null;
+    protected $uses         = null;
+    protected $prefix       = null;
 
-    function __construct($code) {
-        $this->code = $code;
-        $this->setBaseUrl(self::makeBaseUrl($this->code));
-    }
-
-    static protected function makeBaseUrl($as) {
-        $parse = self::parseRouteName($as);
-        $as    = Arr::get($parse, 'as');
-        $as    = str_replace(['.', '_'], ['/', '-'], $as);
-
-        return $as;
+    /**
+     * @return null
+     */
+    public function getPrefix() {
+        return $this->prefix;
     }
 
     /**
-     * @param $as
+     * @param null $prefix
      *
+     * @return Route;
+     */
+    public function setPrefix($prefix) {
+        $this->prefix = $prefix;
+
+        return $this;
+    }
+
+    function __construct($code) {
+        if(strpos($code, '::') !== false) {
+            $this->as        = Arr::get(explode('::', $code), 1);
+            $this->namespace = '\\' . trim(Str::studly(Arr::get(explode('::', $code), 0)), '\\') . '\Controllers';
+        } else {
+            $this->as        = $code;
+            $this->namespace = '\\' . \App::getNamespace() . 'Http\Controllers';
+        }
+        $this->base_url = '/' . trim(str_replace(['.', '_'], ['/', '-'], $this->as), '/') . '/';
+//        dump('-----------',$code, $this->as, $this->base_url);
+    }
+
+    /**
      * @return array
      */
-    static function parseRouteName($as) {
-        if(strpos($as, '::') !== false) {
-            $namespace = Arr::get(explode('::', $as), 0);
-            $as        = Arr::get(explode('::', $as), 1);
-        } else {
-            $namespace = \App::getNamespace();
+    public function getPatterns() {
+        return $this->patterns;
+    }
+
+    function getNamespace() {
+        return $this->namespace;
+    }
+
+    function getAs() {
+        $ret = $this->as;
+        foreach($this->segments as $segment_name => $segment) {
+            $ret .= '.' . str_replace(['{', '}', '?'], '', $segment_name);
         }
 
-        return compact('namespace', 'as');
+        return $ret;
+    }
+
+    public function getBaseUrl() {
+        return $this->base_url;
+    }
+
+    /**
+     * @param $base_url
+     *
+     * @return $this
+     */
+    public function setBaseUrl($base_url) {
+        $this->base_url = '/' . trim($base_url, '/') . '/';
+        $this->base_url = str_replace('//', '/', $this->base_url);
+
+        return $this;
+    }
+
+    public function getUrl() {
+        $url = $this->base_url;
+        foreach($this->segments as $segment_name => $segment) {
+            $url .= $segment . '/';
+        }
+        if('/' != $url) {
+            $url = rtrim($url, '/');
+        }
+
+        return $url;
     }
 
     /**
@@ -60,7 +117,7 @@ class Route {
      *
      * @return Route
      */
-    static function group($as) {
+    static function item($as) {
         if(!isset(self::$routes[$as])) {
             self::$routes[$as] = new Route($as);
         }
@@ -68,45 +125,39 @@ class Route {
         return self::$routes[$as];
     }
 
-    /**
-     * @return RouteItem
-     */
-    function routeIndex() {
-        return $this->route($this->code)->setUrl($this->getBaseUrl());
-    }
-
-    /**
-     * @param $as
-     *
-     * @return RouteItem
-     */
-    protected function route($as) {
-        $r = new RouteItem($as);
-
-        return $r->setUrl(self::makeBaseUrl($as))
-            ->setPattern($this->getModelPattern())
-            ->setModelName($this->getModelName())
-            ->setController(self::makeController($as))
-            ->addMiddleware($this->getMiddleware())
-            ->setAction('index')
-            ->setNamespace($this->getNamespace())
-            ->setDomain($this->getDomain())
-            ->addMiddleware($this->getMiddleware())
-            ->setController(self::makeController($as));
-    }
-
-    static protected function makeController($as) {
-        $parse = self::parseRouteName($as);
-        $as    = Arr::get($parse, 'as');
-
-        return Str::studly(str_replace('.', '_', $as));
-    }
+//    /**
+//     * @param null $route
+//     *
+//     * @return array|mixed
+//     */
+//    public static function getRouteParams($route = null) {
+//        return $route ? Arr::get(self::$route_params, $route, []) : self::$route_params;
+//    }
+//
+//    /**
+//     * @param $route
+//     * @param $params
+//     */
+//    public static function setRouteParams($route, $params) {
+//        self::$route_params[$route] = $params;
+//    }
 
     /**
      * @return null
      */
-    public function getNamespace() {
-        return $this->namespace ? : Route::makeNamespace($this->code);
+    public function getUses() {
+        return $this->uses ? : $this->getNamespace() . '\\' . $this->getController() . '@' . $this->getAction();
+    }
+
+    /**
+     * @param null $uses
+     *
+     * @return Route;
+     */
+    public function setUses($uses) {
+        $this->uses = $uses;
+
+        return $this;
     }
 
     /**
@@ -115,21 +166,72 @@ class Route {
      * @return $this
      */
     public function setNamespace($namespace) {
-        $this->namespace = $namespace;
+        $this->namespace = '\\'.trim($namespace, '\\');
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getController() {
+        $controller = Str::studly(str_replace('.', '_', $this->getAs()));
+
+        return $this->controller ? : $controller . 'Controller';
+    }
+
+    /**
+     * @param null $controller
+     *
+     * @return Route;
+     */
+    public function setController($controller) {
+        $controller = Str::studly($controller);
+        if('Controller' != substr($controller, -10)) {
+            $controller = $controller . 'Controller';
+        }
+
+        $this->controller = $controller;
 
         return $this;
     }
 
-    static function makeNamespace($as) {
-        $parse = self::parseRouteName($as);
-//        dump($parse);
-        $namespace             = Arr::get($parse, 'namespace');
-        $section_namespace     = explode('-', $namespace);
-        $last                  = array_pop($section_namespace);
-        $section_namespace     = implode('-', $section_namespace);
-        $section_namespace_php = trim(Str::studly($section_namespace) . '\\' . Str::studly($last), '\\');
+    /**
+     * @return mixed
+     */
+    public function getAction() {
+        return $this->action ? : 'index';
+    }
 
-        return $section_namespace_php . (($namespace == \App::getNamespace()) ? '\Http\Controllers' : '\Controllers');
+    /**
+     * @param mixed $action
+     *
+     * @return Route;
+     */
+    public function setAction($action) {
+        $this->action = Str::camel($action, '_');
+
+        return $this;
+    }
+
+    function addSegment($name) {
+        $this->segments[$name] = $name;
+
+        return $this;
+    }
+
+    function addPattern($name, $pattern = true) {
+        if(true === $pattern) {
+            $pattern = '[0-9]+';
+        }
+        $this->patterns[$name] = $pattern;
+
+        return $this;
+    }
+
+    function clearSegments() {
+        $this->segments = [];
+
+        return $this;
     }
 
     /**
@@ -154,7 +256,7 @@ class Route {
      * @return array
      */
     public function getMiddleware() {
-        return $this->middleware;
+        return array_values($this->middleware);
     }
 
     /**
@@ -162,129 +264,58 @@ class Route {
      *
      * @return $this
      */
-    public function setMiddleware($middleware) {
-        $this->middleware = $middleware;
+    public function addMiddleware($middleware) {
+        $this->middleware[$middleware] = $middleware;
 
         return $this;
     }
 
-    /**
-     * @return null
-     */
-    public function getBaseUrl() {
-        return $this->base_url;
-    }
-
-    /**
-     * @param $base_url
-     *
-     * @return $this
-     */
-    public function setBaseUrl($base_url) {
-        $this->base_url = '/' . trim($base_url, '/') . '/';
+    public function clearMiddleware() {
+        $this->middleware = [];
 
         return $this;
     }
 
-    /**
-     * @return RouteItem
-     */
-    function routeIndexItem() {
-        return $this->route($this->code . '.item')->setUrl($this->getBaseUrl() . '{' . $this->getModelName() . '}/');
-    }
-
-    /**
-     * @return RouteItem
-     */
-    function routeIndexMethod($method) {
-        return $this->route($this->code . '.' . $method)->setUrl($this->getBaseUrl() . $method);
-    }
-
-    /**
-     * @return RouteItem
-     */
-    function routeIndexItemMethod($method) {
-        return $this->route($this->code . '.item.' . $method)->setUrl($this->getBaseUrl() . '{' . $this->getModelName() . '}/' . $method);
-    }
-
-    public function getModelName() {
-        return $this->getModel('model_name') ? : 'id';
-    }
-
-    public function getModel($param = null) {
-        if($param) {
-            return Arr::get($this->model, $param);
+    public static function normalizeMethod($method) {
+        $method = mb_strtolower($method);
+        if(!in_array($method,
+            [
+                Route::METHOD_DELETE,
+                Route::METHOD_PATCH,
+                Route::METHOD_GET,
+                Route::METHOD_POST,
+                Route::METHOD_PUT,
+            ])
+        ) {
+            $method = 'any';
         }
 
-        return $this->model;
+        return $method;
     }
 
     /**
-     * @param      $model_name
-     * @param      $model_class
-     * @param bool $pattern
+     * @param null $http_method
      *
      * @return $this
      */
-    public function setModel($model_name, $model_class, $pattern = true) {
-        \Route::model($model_name,
-            $model_class,
-            function ($id) {
-                $e = new ModelNotFoundException('Ничего не найдено');
-
-                return $e->setModel($id);
-            });
-
-        $this->model = compact('model_name', 'model_class', 'pattern');
+    function put($http_method = null,$ext=null) {
+        \Route::group([
+            'middleware' => $this->getMiddleware(),
+            'domain'     => $this->getDomain(),
+            'prefix'     => $this->getPrefix(),
+        ], function () use ($http_method, $ext) {
+            $method = self::normalizeMethod($http_method);
+            /** @var $route \Illuminate\Routing\Route */
+            $route = \Route::$method($this->getUrl().($ext?'.'.$ext:''), [
+                'as'   => $this->getAs(),
+                'uses' => $this->getUses(),
+            ]);
+            if(count($this->getPatterns())) {
+                $route->where($this->getPatterns());
+            }
+        });
 
         return $this;
-    }
-
-    public function getModelClass() {
-        return $this->getModel('model_class');
-    }
-
-    public function getModelPattern() {
-        return $this->getModel('pattern') ? : true;
-    }
-
-    static $route_params = [];
-    static $route_seo = [];
-
-    /**
-     * @param null $route
-     *
-     * @return array|mixed
-     */
-    public static function getRouteSeo($route=null) {
-        return $route ? Arr::get(self::$route_seo, $route, []) : self::$route_seo;
-    }
-
-    /**
-     * @param      $route_seo
-     * @param      $title
-     * @param null $description
-     * @param null $keywords
-     */
-    public static function setRouteSeo($route_seo, $title, $description=null, $keywords=null) {
-        self::$route_seo[$route_seo] = compact('title', 'description', 'keywords');
-    }
-
-    /**
-     * @param null $route
-     *
-     * @return array|mixed
-     */
-    public static function getRouteParams($route = null) {
-        return $route ? Arr::get(self::$route_params, $route, []) : self::$route_params;
-    }
-
-    /**
-     * @param $route
-     * @param $params
-     */
-    public static function setRouteParams($route, $params) {
-        self::$route_params[$route] = $params;
     }
 
 }
